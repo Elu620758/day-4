5bf13cb0f11810babc34fac12e0039096795cf40
Elu620758 <elumalai620758@gmail.com>
2023-05-24 00:37:25 +0530

    first commit

---
+++ programs in arrow function/c. Sum of all numbers in an array.js
@@ -0,0 +1,15 @@
+           
+           ((num)=>{
+                
+                sum = 0;
+
+                for(let i = 0; i<num.length; i++)
+                {
+                
+                    sum += num[i];
+                
+                }
+                
+                console.log(sum)
+
+            })([1,2,3,4,5,32,12,32,55])
